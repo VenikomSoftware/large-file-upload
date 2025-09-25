eaced7ecfa46a9424b37b76674a639792039953b
Martin Bojmaliev <mbojmaliev@gmail.com>
2025-09-25 22:04:32 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,17 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
+// Route::middleware(['auth'])->group(function () {
     Route::get('/file-upload-test', FileUploadTest::class);
-    Route::redirect('settings', 'settings/profile');
+    // Route::redirect('settings', 'settings/profile');
 
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-});
+    // Route::get('settings/profile', Profile::class)->name('settings.profile');
+    // Route::get('settings/password', Password::class)->name('settings.password');
+    // Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+// });
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
